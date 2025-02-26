5217b30007e22746b388b01d0772a1c19e51ef82
joaodinizaraujo <joaovictorclassroom@gmail.com>
2025-02-26 13:13:06 -0300

    add root path

--- src/app.py
+++ src/app.py
@@ -4,11 +4,14 @@ from src.routes import user
 
 # base.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="FAAAALA ALEX", description="quero me mata")
-
+app = FastAPI(
+    title="FAAAALA ALEX",
+    description="quero me mata",
+    root_path="/api/v1"
+)
 app.include_router(user.router)
 
 
 @app.get("/")
 def keep_alive():
-    return {"message": "bom dia."}
+    return {"message": "calabreso"}
