39282e9af396476c491aff74a0fadeafbc4b6f42
João Diniz Araujo <joaovictorclassroom@gmail.com>
2025-03-12 15:06:31 -0300

    fix cors

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import user
 
@@ -9,7 +10,18 @@ app = FastAPI(
     description="quero me mata",
     root_path="/api/v1"
 )
+
 app.include_router(user.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
