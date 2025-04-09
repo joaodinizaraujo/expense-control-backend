de72d5ffc9c8ebe4e88c841f8a1bc9a4243af6fe
joaodinizaraujo <joaovictorclassroom@gmail.com>
2025-04-09 08:49:40 -0300

    fix date validation

--- src/schemas/goals.py
+++ src/schemas/goals.py
@@ -2,12 +2,12 @@ from datetime import date, datetime
 from decimal import Decimal
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class GoalsBase(BaseModel):
     ds_title: str = Field(..., description="Goal's title", max_length=100)
-    dt_end: date = Field(..., description="Goal's end date", le=date.today())
+    dt_end: date = Field(..., description="Goal's end date", ge=date.today())
     vl_goal: Decimal = Field(..., description="Goal's value", max_digits=10, decimal_places=2, gt=0)
 
 
@@ -19,4 +19,5 @@ class GoalsResponse(GoalsBase):
     id: int = Field(..., description="Goal's database ID")
     ts_created_at: datetime = Field(..., description="Goal's creation datetime")
     ts_updated_at: Optional[datetime] = Field(None, description="Goal's last update datetime")
-    
+
+    model_config = ConfigDict(from_attributes=True)
